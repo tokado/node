71424acc1a9d127841586e61a8a4dc40a0c11147
Kio Tokado <ole.chaykovskiy@gmail.com>
2022-02-17 13:03:12 +0200

    1hw

---
+++ 1hw/app.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+const path = require('path')
+// Cтворення папок
+// fs.mkdir(path.join(__dirname,'main'), (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err;
+//     }
+// } )
+//
+// fs.mkdir(path.join(__dirname,'main','online'), (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err;
+//     }
+// } )
+//
+// fs.mkdir(path.join(__dirname,'main','inPerson'), (err) => {
+//     if (err) {
+//         console.log(err);
+//         throw err;
+//     }
+// } )
+
+let people = [
+    {
+        name:'Andriy',
+        age:22,
+        city:'Lviv'
+    },
+    {
+        name:'Anton',
+        age:23,
+        city:'Lviv'
+    }
+]
+
+fs.writeFile(path.join(__dirname,'main/inPerson','inPerson.txt'), people[0] , (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+} )
+
+fs.writeFile(path.join(__dirname,'main/online','online.txt'), people[1] , (err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+} )
